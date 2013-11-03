937edba0f79414110d0dae105d81ff99e425ad6d
Constantin <mitides.constantin@gmail.com>
2013-11-02 20:40:12 -0400

    added fixed sizes before removing mq

---
+++ scripts/effects.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+// 	$(".fag").css({marginLeft:'-999px'}).animate(
+// 		{marginLeft: '0px'},{duration: 2000, queue:false});
+// 	$(".jamey").css({marginRight:'-999px'}).animate(
+// 		{marginRight:'0px'},{duration:2000, queue: false});
+// 	// $('#overlay').animate({ opacity: 1, 
+// 	// }, 5000, function(){
+// 	// 		// done fading.
+// 	// });
+// });
