8e123fa24eca0b33b1dab0e07e3c3c43d7726ac5
Constantin <mitides.constantin@gmail.com>
2013-11-04 21:36:19 -0500

    got jquery animation for jamey to work and added a setTimeout

--- scripts/effects.js
+++ scripts/effects.js
@@ -1,16 +1,22 @@
 $(document).ready(function(){
-// 	$(".fag").css({marginLeft:'-999px'}).animate(
-// 		{marginLeft: '0px'},{duration: 2000, queue:false});
-// 	$(".jamey").css({marginRight:'-999px'}).animate(
-// 		{marginRight:'0px'},{duration:2000, queue: false});
-// 	// $('#overlay').animate({ opacity: 1, 
-// 	// }, 5000, function(){
-// 	// 		// done fading.
-// 	// });
+	setTimeout(function(){
+		$(".fag").delay(5000).css({marginLeft:'-999px'}).animate(
+			{marginLeft: '0px'},{duration: 2000, queue:false});
+		$(".jamey").css({left:'999px'}).animate(
+			{left:'606px'},{duration:2000, queue: false});
+		$(".jamey2").css({left:'999px'}).animate(
+			{left:'606px'},{duration:2000, queue: false});
+		$(".rip-date").css({left:'999px'}).animate(
+			{left:'375px'}, {duration:2000, queue: false});
+		$('#overlay').animate({ opacity: 1, }, 5000, function(){
+				// done fading.
+		});
+		$(".fag").css({color:'#191919'});
+	}, 5000);
 				//js modal //
-	onclick=function overlay(){
-		el= document.getElementById("overlay-modal");
-		el.style.visibility = (el.style.visibility == "visible")? "hidden":"visible";
-	};
+	// onclick=function overlay(){
+	// 	el= document.getElementById("overlay-modal");
+	// 	el.style.visibility = (el.style.visibility == "visible")? "hidden":"visible";
+	// };
 
  });
